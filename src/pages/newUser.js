876217c62a47d382b695ad6c7851ecb835d34c69
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-01 20:23:15 -0700

    something in the wrong place

--- src/pages/newUser.js
+++ src/pages/newUser.js
@@ -4,6 +4,7 @@ const Signup = () => {
 
     // render the logo (component)
     // render the name input box
+    
     // render the username (component)
     // render the password (component)
     // button component with props (create new Account)(Already a Member)passed in for each button
