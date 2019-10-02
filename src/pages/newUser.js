1658dcfdf1b9620b7b8d15baf41e748cec461bbb
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-01 20:02:24 -0700

    psuedo code added to the signup component

--- src/pages/newUser.js
+++ src/pages/newUser.js
@@ -2,4 +2,10 @@ import React from 'react';
 
 const Signup = () => {
 
+    // render the logo (component)
+    // render the name input box
+    // render the username (component)
+    // render the password (component)
+    // button component with props (create new Account)(Already a Member)passed in for each button
+
 }
