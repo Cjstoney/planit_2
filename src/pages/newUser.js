f414f57df6527793d46d32d73ffaa15fec81f41b
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-22 16:47:24 -0700

    payload strted for a new user.  Need password encryption still

--- src/pages/newUser.js
+++ src/pages/newUser.js
@@ -3,6 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Link} from 'react-router-dom';
 
+
+// function to capture all inputs and package it up to send to db
+
+const newUserPayload = () =>{
+    let newName = document.getElementsByName('name')[0].value;
+    let newemail = document.getElementsByName('email')[0].value;
+    let newpassword = document.getElementsByName('password')[0].value; //need to hash with bcrypt?
+    let userPayload = {
+        Name:newName,
+        email:newemail,
+        password:newpassword
+    }
+    console.log(userPayload)
+}
+
+
+
 class Signup extends React.Component {
     render() {
         return(
@@ -14,22 +31,22 @@ class Signup extends React.Component {
                 {/* render the name input box */}
                 <Form.Group controlId="formName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="name" placeholder="Enter your name" />
+                    <Form.Control name="name" type="name" placeholder="Enter your name" />
                 </Form.Group>
                 {/* render the username */}
                 <Form.Group controlId="formUserName">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control name='email' type="email" placeholder="Enter email" />
                 </Form.Group>
                 {/* render the password */}
                 <Form.Group controlId="formGroupPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" />
                 </Form.Group>
             </Form>
 
             {/* button component with props passed in for each button */}
-            <Button variant="outline-success" className="createNewUser">Create User</Button>
+            <Button variant="outline-success" className="createNewUser" onClick={newUserPayload}>Create User</Button>
             <Link to="/loginComponent"> Already a member</Link>
 
         </div>
