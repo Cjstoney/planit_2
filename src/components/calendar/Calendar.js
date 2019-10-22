194439b7c7be2a97c61b19448469ca9706e75c3d
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-22 13:12:16 -0700

    componentDidMount added and works for initial get of the date. On click of a second date generates issues

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,46 +1,42 @@
 import React from "react";
-import Calendar from 'react-calendar'
+import Calendar from "react-calendar";
 
 
+const getDate = (input)=>{
+   let dateString = input.toString();
+   let dateArray = dateString.split(" ");
+   let payload = {
+     dayOfWeek: dateArray[0],
+     month: dateArray[1],
+     dayOfMonth: dateArray[2],
+     year: dateArray[3]
+   };
+   console.log(payload);
+   return payload;
+  }
 
 class CalendarComp extends React.Component {
-    
+  
     state = {
-        value: new Date()
-    }
-    
+      value: new Date()
+    };
+  
+    onChange = value => this.setState({ value });
+
+  componentDidMount(){
+    getDate(this.state.value)
+  }
+  
+  render() {
+  
     
 
-    onChange = value => this.setState({ value })
-    
-   
-    render() {
-
-        const {value} = this.state; //assiging the state to a variable to have access to it in later functions
-
-        // parses through the date info and prepares it to be used as a paylod to fetch events for that day
-        const getDate = () =>{
-            let dateString = value.toString();
-            let dateArray = dateString.split(" ")
-            let payload ={
-                dayOfWeek : dateArray[0],
-                month : dateArray[1],
-                dayOfMonth : dateArray[2],
-                year: dateArray[3]
-            }
-            console.log(payload)
-            return payload 
-        }
-        return (
-            <div onClick={getDate}>
-                
-              <Calendar 
-                onChange={this.onChange}
-                value={this.state.dat}
-              />
-            </div>
-          );
-        }
+    return (
+      <div onClick={getDate}>
+        <Calendar onChange={this.onChange} value={this.state.date} />
+      </div>
+    );
+  }
 }
 
 export default CalendarComp;
