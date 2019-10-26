f85abd4da537c126c263a9cff8d73b1af734efab
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-26 11:31:27 -0700

    rough login validation and payload created.

--- src/pages/loginComponent.js
+++ src/pages/loginComponent.js
@@ -2,6 +2,30 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+const emailValidation=(email)=>{
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+
+const loginPayload=()=>{
+    let loginUserPayload={};
+    let ogLoginEmail = document.getElementsByName('email')[0].value;
+    let loginPassword = document.getElementsByName('password')[0].value;
+    let loginEmail = ogLoginEmail.toLowerCase();
+    if (emailValidation(loginEmail) && loginPassword.length>=7){
+        loginUserPayload={
+            email:loginEmail,
+            password:loginPassword
+        }
+        console.log('loginUserPayload ', loginUserPayload)
+    }else if(emailValidation(loginEmail) && loginPassword.length<7){
+        alert("password is not long enough")
+    }else if( emailValidation(loginEmail) === false && loginPassword.length>7){
+        alert('not a valid email')
+    }else{
+        alert('email and password are not valid')
+    }
+}
+
 class LoginComponent extends React.Component {
     render() {
         return(
@@ -13,17 +37,17 @@ class LoginComponent extends React.Component {
                 {/* render the username */}
                 <Form.Group controlId="formUserName">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control name="email" type="email" placeholder="Enter email" />
                 </Form.Group>
                 {/* render the password */}
                 <Form.Group controlId="formGroupPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control name='password' type="password" placeholder="Password" />
                 </Form.Group>
             </Form>
 
             {/* button component with props passed in for each button */}
-            <Button variant="outline-primary" className='login'>Login</Button>
+            <Button onClick={loginPayload} variant="outline-primary" className='login'>Login</Button>
 
         </div>
         )
