9ccbff8cb16608cfe66056adbf5fae26ddf2ba4d
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-30 15:09:33 -0700

    sanity check on restful api works

--- server.js
+++ server.js
@@ -3,5 +3,11 @@ const path = require('path')
 const routes = require('./routes/apiRoutes.js')
 var app = express();
 
-app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+routes(app);
+
+let server = app.listen(3001, function (){
+    console.log('app running on port. ', server.address().port)
+})
