b8512ef432f85d1abeb0ba8dcad251266cb786c7
Camerone Stoney <Cameronestoney@yahoo.com>
2019-11-12 12:44:09 -0800

    taking sequelize out of the picture since it continues to break things. Will attempt with just sql queries

--- schema.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS calendarDB;
-CREATE DATABASE calendarDB;
-
-
-
--- server.js
+++ server.js
@@ -1,28 +1,37 @@
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const db = require("./models");
+const bodyParser = require("body-parser");
 
-const express = require('express');
-const path = require('path')
-var app = express();
-var db = require('./models')
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
+}
 
-const PORT = process.env.PORT || 3001
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-let syncOptions = { force: false }
+require("./routes/apiRoutes")(app);
 
-if (process.env.NODE_ENV === 'test'){
+let syncOptions = { force: false };
+
+// If running a test, set syncOptions.force to true
+// clearing the `testdb`
+if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-require('./routes/apiRoutes')(app)
-
-db.sequelize.sync(syncOptions).then(function(){
-  app.listen(PORT, function() {
-      console.log(
-        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-        PORT,
-        PORT
-      );
-    });
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build","index.html"));
 });
+
+  app.listen(PORT, 
+    console.log(
+      "==> 🌎  Listening on port %s.",
+      PORT
+    )
+  );
