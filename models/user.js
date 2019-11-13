b717b29f973784d2562e8dfdec39d42b65e42697
Camerone Stoney <Cameronestoney@yahoo.com>
2019-11-12 22:06:07 -0800

    User model created. Still need association later

---
+++ models/user.js
@@ -0,0 +1,22 @@
+module.exports = function (sequelize, DataTypes){
+    const User = sequelize.define('Users', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+          },
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          password: {
+            type: DataTypes.STRING,
+            allowNull: false
+          }
+    });
+    return User;
+}
