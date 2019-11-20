2321646daca7c15a8e7f3eb3cb5f716053039cf3
Camerone Stoney <Cameronestoney@yahoo.com>
2019-11-20 13:12:22 -0800

    formatting changes

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,34 +1,5 @@
 import React from "react";
-import axios from 'axios';
-
-/*const getDate = input => {
-  let dateString = input.toString();
-  let payload = {};
-  let dateArray = dateString.split(" ");
-
-  if (dateString.length > 20) {
-    payload = {
-      dayOfWeek: dateArray[0],
-      month: dateArray[1],
-      dayOfMonth: dateArray[2],
-      year: dateArray[3]
-    };
-    // console.log("todo ", payload);
-    axios
-    .post("http://localhost:3001/api/month", {
-      monthPayload
-    })
-    .then(response => {
-      console.log(response, "response");
-      return response;
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-};*/
- 
-
+import axios from "axios";
 
 class ToDo extends React.Component {
   constructor(props) {
@@ -39,61 +10,57 @@ class ToDo extends React.Component {
   }
 
   componentDidMount() {
-   
-   console.log(this.props.day) 
+    console.log(this.props.day);
   }
 
   componentDidUpdate(prevProps) {
-    if (this.props.day!== prevProps.day) {
-      let stringDate = this.props.day.toString()
-      const array = stringDate.split(" ")
-      const user = window.localStorage.getItem('uid')
-      let payload={}
-      console.log(array, 'array')
+    if (this.props.day !== prevProps.day) {
+      let stringDate = this.props.day.toString();
+      const array = stringDate.split(" ");
+      const user = window.localStorage.getItem("uid");
+      let payload = {};
+      console.log(array, "array");
 
-     payload = {
+      payload = {
         month: array[1],
         year: array[3],
         user: user
       };
       console.log("todo ", payload);
       axios
-      .post("http://localhost:3001/api/month", {
-       payload
-      })
-      .then(response => {
-        // console.log(response, "response");
-        this.setState({
-          items: response.data
+        .post("http://localhost:3001/api/month", {
+          payload
         })
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+        .then(response => {
+          // console.log(response, "response");
+          this.setState({
+            items: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
-  
-  
+  }
 
   render() {
-    let postItem = this.state.items?(
-      <ul className= 'postItem'>
-        {
-          this.state.items.map(function(e, idx, arr){
-            return(
-              <li className='returnedEvents' key={e.Event_id} id={e.Event_id}>
-                {e.name}
-                <div className='itemDate'>{e.month}</div>
-                <div className='itemDate'>{e.day}</div>
-                <div className='itemDate'>{e.year}</div>
-                <div className='itemDescription'>{e.description}</div>
-                
-                </li>
-            )
-          }
-          )}
+    let postItem = this.state.items ? (
+      <ul className="postItem">
+        {this.state.items.map(function(e, idx, arr) {
+          return (
+            <li className="returnedEvents" key={e.Event_id} id={e.Event_id}>
+              {e.name}
+              <div className="itemDate">{e.month}</div>
+              <div className="itemDate">{e.day}</div>
+              <div className="itemDate">{e.year}</div>
+              <div className="itemDescription">{e.description}</div>
+            </li>
+          );
+        })}
       </ul>
-    ):(<h1>Nothing to See here</h1>)
+    ) : (
+      <h1>Nothing to See here</h1>
+    );
     return (
       // create a div that will have the title of "todo"
       // needs to have a add new todo button on the bottom of the container
