94927377083fe0cad2b04b7f0c3e44129c6a500e
Camerone Stoney <Cameronestoney@yahoo.com>
2019-11-20 16:13:14 -0800

    form added to the component. Going to accept props now

--- src/components/AddEvent.js
+++ src/components/AddEvent.js
@@ -1,4 +1,5 @@
-
+import React from 'react'
+import Form from 'react-bootstrap/Form'
 
 class AddEvent extends React.Component {
     constructor(props){
@@ -6,6 +7,31 @@ class AddEvent extends React.Component {
      this.state = {
 
      }
+    }
+
+    render(){
+        return(
+            <div className='add-event=container' >
+                 <Form>
+          {/* render the event name */}
+          <Form.Group controlId="formEventName">
+            <Form.Label>Event Name</Form.Label>
+            <Form.Control name="event-name" type="text" placeholder="Event Name" />
+          </Form.Group>
+          <Form.Group controlId="formEventDesc">
+            <Form.Label>Event Description</Form.Label>
+            <Form.Control name="event-Description" type="text" placeholder="Event Description" />
+          </Form.Group> 
+        </Form>
+        <Button
+          onClick={}
+          variant="outline-primary"
+          className="login"
+        >
+          Add Event
+        </Button>
+            </div>
+        )
     }
 }
 export default AddEvent;
