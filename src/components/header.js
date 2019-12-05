c2ace9b1edb9cb663101bcb598779d743c4c0aaf
Camerone Stoney <Cameronestoney@yahoo.com>
2019-12-04 19:50:30 -0800

    added the function to clear localstorage to the button

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 
 function header() {
+const logout = ()=>{
+    if(localStorage.getItem('uid')!== null){
+        localStorage.clear()
+    }
+}
+
     return (
         <div>
             <h1 className='app-title'>Planit</h1>
-            <Button variant="outline-success" className="newUser-button" id="createNewUser" onClick={newUserPayload}>Create User</Button>
+            <Button variant="outline-success" className="newUser-button" id="createNewUser" onClick={logout}>Create User</Button>
         </div>
     )
 }
